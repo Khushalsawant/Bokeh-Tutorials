30f1e562d5a5f8a043d1aeda63cfae609065f23d
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2021-07-18 13:15:25 +0530

    Update radiation_equation.py
    Added graph generation thru matplotlib module

--- radiation_equation.py
+++ radiation_equation.py
@@ -2,6 +2,8 @@ import logging
 from random import randint
 from bokeh.plotting import figure, show,output_file
 from bokeh.models import HoverTool, NumeralTickFormatter,SaveTool
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 """ (W/m2K4) - The Stefan-Boltzmann Constant"""
 sb_constant = 5.6703 * pow(10,-8)
@@ -127,6 +129,62 @@ def calculate_heat_trasnfer(constant_field='Temperature',):
     else:
         LOG.info('Invalid Constant field is entered, value is {0}'.format(constant_field))
 
+def calculate_heat_trasnfer_maptplotlb(constant_field='Temperature'):
+    LOG.debug('calculate_heat_trasnfer')
+    #area_list = [randint(9**(9-i), (9**9)-i) for i in range(0,10)]
+    area_list = [ (10**5)*i for i in range(1,11)]
+    if constant_field == 'Temperature':
+        hf = []
+        #Temp = 6000 # Temp of Sun in kelvin
+        Temp = 288 # Temp of Earth in kelvin
+        for ar_m2 in area_list:
+            q = sb_constant* Temp * ar_m2
+            hf.append(q)
+        hf_data = dict(zip(area_list,hf))
+        LOG.debug('graphical_data_plotting')
+        x = list(hf_data.keys())
+        y = list(hf_data.values())
+        LOG.info('values for x axis= {0}'.format(x))
+        LOG.info('values for y axis= {0}'.format(y))
+        fig, ax = plt.subplots()
+        ax.plot(x, y,label='Area vs Heat Transfer')
+        ax.scatter(x, y,label='Area vs Heat Transfer')
+        ax.set_xlabel("Area in M2")  # add X-axis label
+        ax.set_ylabel("Heat Transfer per Unit time")  # add Y-axis label
+        ax.set_title("Area vs Heat Transfer")  # add title
+        ax.xaxis.set_major_formatter(FormatStrFormatter('% 1.2f'))
+        #ax.yaxis.set_major_formatter(FormatStrFormatter('% 1.2f'))
+        ax.grid(True)
+        plt.savefig("Area vs Heat Transfer.png")
+        plt.show()
+    elif constant_field=='heat_transfer':
+        hf = []
+        q=15
+        for ar_m2 in area_list:
+            Temp = q/(sb_constant* ar_m2)
+            hf.append(Temp)
+        hf_data = dict(zip(area_list,hf))
+        LOG.debug('graphical_data_plotting')
+        x = list(hf_data.keys())
+        y = list(hf_data.values())
+
+        LOG.info('values for x axis= {0}'.format(x))
+        LOG.info('values for y axis= {0}'.format(y))
+        fig, ax = plt.subplots()
+        ax.plot(x, y,label='Area vs Heat Transfer')
+        ax.scatter(x, y,label='Area vs Heat Transfer')
+        ax.set_xlabel("Area in M2")  # add X-axis label
+        ax.set_ylabel("Temperature")  # add Y-axis label
+        ax.set_title("Area vs Temperature ")  # add title
+        ax.xaxis.set_major_formatter(FormatStrFormatter('% 1.2f'))
+        #ax.yaxis.set_major_formatter(FormatStrFormatter('% 1.2f'))
+        ax.grid(True)
+        plt.savefig("Area vs Temperature.png")
+        plt.show()
+
 if __name__ == '__main__':
+    pass
     #calculate_heat_trasnfer('heat_transfer')
-    calculate_heat_trasnfer('Temperature')
+    #calculate_heat_trasnfer_maptplotlb(constant_field='Temperature')
+    #calculate_heat_trasnfer_maptplotlb(constant_field='heat_transfer')
+    #calculate_heat_trasnfer('Temperature')
